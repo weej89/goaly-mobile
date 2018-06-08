e020e84b1a362f29d20604769ef43e5782f5b506
Landon <weej89@gmail.com>
2018-06-07 21:34:15 -0600

    Initial Commit
    First Goaly Commit!

---
+++ GoalyMobile/GoalyMobile/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoalyMobile.Models
+{
+    public class TodoItem
+    {
+        public string Id { get; set; }
+        
+        public string Text { get; set; }
+    }
+}
---
+++ GoalyMobile/GoalyMobile/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GoalyMobile.Views
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
