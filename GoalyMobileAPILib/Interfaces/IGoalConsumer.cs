3945f9613fd162d9501e8e65f850e8c217403cd1
Landon <weej89@gmail.com>
2018-06-07 23:08:49 -0600

    Test and API Projects
    API interface project added
    UI and Unit Test projects added

---
+++ GoalyMobileAPILib/Interfaces/IGoalConsumer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoalyMobileAPILib.Interfaces
+{
+    public interface IGoalConsumer
+    {
+    }
+}
---
+++ GoalyMobileAPIUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GoalyMobileAPIUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GoalyMobileAPIUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("31bb835e-3ef4-4105-9572-5e2bece75780")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
